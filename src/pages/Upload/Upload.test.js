976ffbf5382d103f07640a9ffee8d1bb244a7e4c
MostafaHMK <mostafahmk@gmail.com>
2021-06-08 14:43:03 +0200

    Complete unit testing

--- src/pages/Upload/Upload.test.js
+++ src/pages/Upload/Upload.test.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import Upload from '../Upload/Upload'
 import { render } from '@testing-library/react'
+import { Provider } from 'react-redux';
+import store from '../../storev2/store';
 
 it ('Renders Upload page correctly', () => {
-    const {getByText} = render(<Upload />)
+    const {getByText} = render(<Provider store={store}>< Upload /></Provider>)
     expect(getByText('Choose photos and videos to upload')).toBeTruthy()
+    expect(getByText('wut')).toBeTruthy()
 })
